8320fc79952cb2d8911e9e2aa4de158be0d5d499
Bogdan Derevyaga <derevyaga.bogdan@fornova.com>
2020-05-14 23:10:27 +0300

    ADD prefix

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
   await app.listen(3000);
-  console.log(`Application is running on: ${await app.getUrl()}`);
+
+  console.info(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
