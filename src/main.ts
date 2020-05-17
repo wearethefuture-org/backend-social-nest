3dd1313bbb7680049ce2f7d47f35a6599e35f5d1
Ihorzharyi <56356305+wearethefuture-org@users.noreply.github.com>
2020-05-18 00:43:12 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,6 @@ async function bootstrap() {
   console.info(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap().finally(() => {
-  console.log('success');
+  console.info('success');
   }
 );
