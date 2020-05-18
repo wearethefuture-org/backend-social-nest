1d184691837e5b53826e41e0732a5409789c7ae7
Bogdan Derevyaga <derevyaga.bogdan@fornova.com>
2020-05-18 22:42:18 +0300

    add migrations

---
+++ ormconfig.js
@@ -0,0 +1,25 @@
+module.exports = {
+  type: 'postgres',
+  host: process.env.DATABASE_HOST,
+  port: parseInt(process.env.DATABASE_PORT),
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  synchronize: true,
+  entities: [
+    `${__dirname}/src/**/*.entity.ts`,
+    `${__dirname}/dist/**/*.entity.js`
+  ],
+  migrations: [
+    `${__dirname}/migrations/**/*.ts`,
+  ],
+  subscribers: [
+    `${__dirname}/subscriber/**/*.ts`,
+    `${__dirname}/dist/subscriber/**/.js`,
+  ],
+  cli: {
+    entitiesDir: 'src',
+    migrationsDir: 'migrations',
+    subscribersDir: 'subscriber'
+  },
+};
