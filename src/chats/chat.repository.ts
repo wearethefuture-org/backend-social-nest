b28dc730624660d0f9396da9a1f247d3b4da814f
NadiaVoloshyna <dnipronadiya@gmail.com>
2020-07-30 20:56:58 +0300

    chat repository added

---
+++ src/chats/chat.repository.ts
@@ -0,0 +1,17 @@
+import { EntityRepository, Repository } from "typeorm";
+import { Chat } from './chats.entity';
+import { GetChatsFilterDto } from './dto/get-chats-filter.dto';
+
+@EntityRepository(Chat)
+export class ChatRepository extends Repository<Chat> {
+    async getChats(filterDto: GetChatsFilterDto): Promise<Chat[]> {
+        const { search } = filterDto;
+        const query = this.createQueryBuilder('chat');
+        
+        if (search) {
+            query.where('name ILIKE :search', { search: `%${search}%`});
+          }
+          const chats = await query.getMany();
+          return chats;
+        }
+      }
