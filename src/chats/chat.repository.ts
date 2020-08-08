dfb6d8849308a0fc7e4729ba7d402750fddad019
NadiaVoloshyna <dnipronadiya@gmail.com>
2020-08-08 14:04:17 +0300

    formatting

--- src/chats/chat.repository.ts
+++ src/chats/chat.repository.ts
@@ -4,15 +4,18 @@ import { GetChatsFilterDto } from './dto/get-chats-filter.dto';
 
 @EntityRepository(Chat)
 export class ChatRepository extends Repository<Chat> {
-    async getChatsOfUser(filterDto: GetChatsFilterDto): Promise<Chat[]> {
-        const { search } = filterDto;
-        const query = this.createQueryBuilder('chat');
-        if (search) {
-            query.where('name ILIKE :search', { search: `%${search}%`});
-          }  
-          const chats = await query.getMany();
-          return chats;
-        }
-      }
+  async getChatsOfUser(filterDto: GetChatsFilterDto): Promise<Chat[]> {
+    const { search } = filterDto;
+    const query = this.createQueryBuilder('chat');
+        
+    if (search) {
+      query.where('name ILIKE :search', { search: `%${search}%`});
+    }  
+
+    const chats = await query.getMany();
+
+    return chats;
+  }
+}
 
      
