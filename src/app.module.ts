59646c725c1a1df6597b947ab72c5dee369cbe69
Movchan94 <tkd.movchan@gmail.com>
2020-08-13 22:03:47 +0300

    merge with dev

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,18 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
-import { FilesModule } from './files/files.module';
 import { ChatsModule } from './chats/chats.module';
 import { AuthModule } from './auth/auth.module';
 import { MessagesModule } from './messages/messages.module'
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: process.env.ROOT_PATH,
+      //rootPath: join(__dirname, '..', '..', 'static', 'qwert'  ),
+      serveRoot: process.env.SERVE_ROOT
+    }),
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
@@ -19,10 +24,10 @@ import { MessagesModule } from './messages/messages.module'
       password: process.env.DATABASE_PASSWORD,
       database: process.env.DATABASE_NAME,
       autoLoadEntities: true,
-      // synchronize: true
+      synchronize: true,
+
     }),
     AuthModule,
-    FilesModule,
     UsersModule,
     ChatsModule,
     MessagesModule,
---
+++ src/utils/fileUpload.utils.ts
@@ -0,0 +1,19 @@
+import { extname } from 'path';
+import { HttpException, HttpStatus } from '@nestjs/common';
+
+export const imageFileFilter = (req, file, callback) => {
+  if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+    return callback(new HttpException('You File incorrect, please use format: jpeg, jpg, png, gif', HttpStatus.BAD_REQUEST), false);
+  }
+  callback(null, true);
+};
+
+export const editFileName = (req, file, callback) => {
+  const fileExtName = extname(file.originalname);
+  const randomName = Array(15)
+    .fill(null)
+    .map(() => Math.round(Math.random() * 16).toString(16))
+    .join('');
+  callback(null, `${randomName}${fileExtName}`);
+};
+
